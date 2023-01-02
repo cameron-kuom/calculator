11f4f43d99319f137a558deb2311a7e7b3d84f34
cameron-kuom <117031925+cameron-kuom@users.noreply.github.com>
2023-01-01 22:15:56 -0500

    Create starting operate logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+const btns = document.querySelectorAll("button");
 const numBtns = document.querySelectorAll(".num");
 const operatorBtns = document.querySelectorAll(".operator");
+const inputsArray = [];
 
+function operate(){
+    btns.forEach(btn => btn.addEventListener("click", () => {
+        inputsArray.push(btn.textContent);
+
+        let operends = inputsArray.join("").split(/[=x+/-]/).map(item => 
+            parseInt(item));
+
+        let operatorArray = inputsArray.filter(input => 
+            ["+","-","x","/", "="].includes(input));
+        let operator = secondLastIndex(operatorArray);
+
+        if (isNaN(operends[2]) && operends.length == 3){
+            if (operator == "+"){
+                let total = addNums(operends[0], operends[1]);
+            } else if (operator == "-"){
+                let total = subtractNums(operends[0], operends[1]);
+            } else if (operator == "x"){
+                let total = multiplyNums(operends[0], operends[1]);
+            } else if (operator == "/"){
+                let total = divideNums(operends[0], operends[1]);
+            }
+        }
+    }))
+}
+
+// Helper functions
 function addNums(x,y){
     return x + y;
 }
@@ -17,14 +45,8 @@ function multiplyNums(x,y){
     return x * y;
 }
 
-function operate(x,y,operator){
-    if (operator = "+"){
-        return addNums(x,y)
-    } else if (operator == "-"){
-        return subtractNums(x,y)
-    } else if (operator == "/"){
-        return divideNums(x,y)
-    } else if (operator == "x"){
-        return multiplyNums(x,y)
-    }
+function secondLastIndex(array){
+    let Index = array.length - 2;
+    let element = array[Index];
+    return element;
 }
